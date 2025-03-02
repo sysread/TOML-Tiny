4a53a93cf76fe77f8a6d73e457a0e6687199e4fe
Ian Jackson <ijackson@chiark.greenend.org.uk>
2025-03-02 21:33:10 +0000

    Writer: Fix param handling
    The Writer doesn't actually seem to have any params right now, but
    we're going to add one.
    
    It appears this code was wrong when it was introduced in 5489d0cf0fa.b
    The author was presumably confused by the renumbering of @_ due to the
    shift just before.
    
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- lib/TOML/Tiny/Writer.pm
+++ lib/TOML/Tiny/Writer.pm
@@ -16,7 +16,7 @@ my @KEYS;
 
 sub to_toml {
   my $data  = shift;
-  my $param = ref($_[1]) eq 'HASH' ? $_[1] : undef;
+  my $param = ref($_[0]) eq 'HASH' ? $_[0] : { @_ };
 
   die 'toml: found undefined value, which is unsupported by TOML' if ! defined $data;
 
